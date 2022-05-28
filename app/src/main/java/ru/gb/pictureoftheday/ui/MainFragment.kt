0562f289473a0309c71b3819920677572a53c528
neqelovek <neqelovek@gmail.com>
2022-05-28 14:09:43 +0300

    Lesson1

--- app/src/main/java/ru/gb/pictureoftheday/ui/MainFragment.kt
+++ app/src/main/java/ru/gb/pictureoftheday/ui/MainFragment.kt
@@ -1,7 +1,13 @@
 package ru.gb.pictureoftheday.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import ru.gb.pictureoftheday.R
 
-class MainFragment: Fragment(R.layout.fragment_layout) {
+class MainFragment : Fragment(R.layout.fragment_layout) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
